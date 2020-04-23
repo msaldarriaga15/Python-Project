c78dceac6a8fa345b513e17954af95bdf111d6da
alinebocamino <58132859+alinebocamino@users.noreply.github.com>
2020-04-23 12:13:47 +0200

    Create setup.sh

---
+++ demo/setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"alinebocamino@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
